86e59add51de2d98091b7c4a7b10c89d63382d8e
Hak-MMD <myhamike@gmail.com>
2025-09-23 13:04:54 -0400

    Server is ready v1

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -16,6 +16,31 @@ const openai = require("../config/openai.js");
 //   }
 // };
 
+// 400/500 status test
+// const message = async (req, res) => {
+//   const { text, screenshot } = req.body;
+//   try {
+//     console.log("Received text:", text);
+//     if (text) {
+//       console.log("error here: ", text);
+//       return res.status(400).json({ errorMessage: `400 1  error textissent` });
+//     } else if (screenshot) {
+//       return res
+//         .status(500)
+//         .json({ errorMessage: `500 2 error screenshot sent` });
+//     }
+//     await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulate processing delay
+//     // console.log("Received screenshots:", screenshot);
+//     res.json({
+//       reply: `Hello from the API controller! ${
+//         screenshot ? "Screenshot received." : "No screenshot."
+//       }`,
+//     });
+//   } catch (error) {
+//     console.log("Error in message controller:", error);
+//   }
+// };
+
 const message = async (req, res) => {
   try {
     const { text, screenshot } = req.body; // accept both text and image URL
@@ -49,12 +74,23 @@ const message = async (req, res) => {
       ],
     });
 
-    res.json({ reply: completion.choices[0].message.content });
+    if (
+      (!completion?.choices[0]?.message?.content ||
+        completion?.choices[0]?.message?.content === "") &&
+      completion?.error
+    ) {
+      return res.status(500).json({
+        errorMessage: "AI error! Try again later!",
+        details: completion.error.message,
+      });
+    }
+    // console.log;
+
+    res.status(200).json({ reply: completion.choices[0].message.content });
   } catch (err) {
     console.error("AI error:", err);
-    res.status(500).json({ error: "Something went wrong" });
+    res.status(500).json({ error: "Something went wrong. Truy again later!" });
   }
 };
 
-// module.exports = { message };
 module.exports = { message };
